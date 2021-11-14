68e7581213443055508061953a4151270ea3ad0f
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-11-13 20:58:49 -0500

    initial commit

---
+++ Assets/Scripts/KeyboardComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardComponent : MonoBehaviour {
+
+	public KMSelectable attachedSelectable;
+	private KeyboardSupportScript parentService;
+	void Awake () {
+		attachedSelectable = GetComponent<KMSelectable>();
+		parentService = FindObjectOfType<KeyboardSupportScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
