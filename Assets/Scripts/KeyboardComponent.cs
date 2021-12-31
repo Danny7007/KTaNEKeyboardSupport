2f065cedf6192e186c2ccaf82e14bc1ec7bf2fe0
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-12-31 13:13:47 -0500

    some progress

--- Assets/Scripts/KeyboardComponent.cs
+++ Assets/Scripts/KeyboardComponent.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class KeyboardComponent : MonoBehaviour {
 
-	public KMSelectable attachedSelectable;
-	private KeyboardSupportScript parentService;
+	private KMSelectable attachedSelectable;
+
+	public List<KeyCode> boundKeys;
+	private bool held; 
+
 	void Awake () {
 		attachedSelectable = GetComponent<KMSelectable>();
-		parentService = FindObjectOfType<KeyboardSupportScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        foreach (KeyCode key in boundKeys)
+        {
+			if (Input.GetKeyDown(key) && !held)
+            {
+				held = true;
+				attachedSelectable.OnInteract();
+            }
+			if (Input.GetKeyUp(key) && held)
+            {
+				held = false;
+				attachedSelectable.OnInteractEnded();
+            }
+        }
 	}
 }
---
+++ Assets/Scripts/ModuleInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleInfo  {
+
+	public string moduleID { get; private set; }
+	public string displayName { get; private set; }
+
+	public KeyboardComponent[] components;
+}
